a89150424abf19f7518355fd175c94cbe5b3cbaf
Shazaniyu55 <shazaniyu@gmail.com>
2024-04-17 13:44:40 +0100

    new about page

--- src/components/team.js
+++ src/components/team.js
@@ -3,7 +3,15 @@ import team1 from '../assets/team/1.png'
 import team2 from '../assets/team/2.png'
 import team3 from '../assets/team/3.png'
 import team4 from '../assets/team/5.png'
-import team5 from '../assets/team/5.png'
+import team7 from '../assets/team/team7.jpeg'
+import team8 from '../assets/team/team8.jpeg'
+import team9 from '../assets/team/team9.jpeg'
+import team10 from '../assets/team/team10.jpeg'
+import team11 from '../assets/team/team11.jpeg'
+import team12 from '../assets/team/team12.jpeg'
+import team13 from '../assets/team/team13.jpeg'
+
+
 import {FaFacebook, FaLinkedinIn} from 'react-icons/fa'
 import { Link } from "react-router-dom";
 
@@ -13,8 +21,8 @@ const TeamCard = ({ imgSrc, name, title }) => {
       <div className=" relative flex flex-col gap-1 md:hover:shadow-lg rounded-xl md:py-10 team-card md:cursor-pointer">
         <img
           src={imgSrc}
-          width={130}
-          height={130}
+        
+          style={{width: '150px', height: '150px'}}
           alt="team member"
           className="drop-shadow-2xl w-20 sm:w-32 md:mb-5 mb-3 rounded-full border-2 border-yellow mx-auto"
         />
@@ -45,18 +53,36 @@ const TeamCard = ({ imgSrc, name, title }) => {
   
         <div className="mx-auto grid grid-cols-2 lg:grid-cols-3 gap-y-8 sm:gap-8 mt-16">
           <TeamCard
-            imgSrc={team1}
-            name="Saimon Harmer"
+            imgSrc={team7}
+            name="Fabian Lawrence"
             title="CEO and Founder"
           />
-          <TeamCard imgSrc={team2} name="Aaron Nunez" title="Founder" />
+          <TeamCard imgSrc={team10} name="Ruth Fabian" title="MD" />
           <TeamCard
-            imgSrc={team3}
-            name="Abdulwahab Ekekhide"
-            title="Web Designer"
+            imgSrc={team12}
+            name="Queeneeth ibeabuchi"
+            title="Secretary"
           />
           <TeamCard
-            imgSrc={team4}
+            imgSrc={team8}
+            name="Lina Jutila"
+            title="Web Developer"
+          />
+
+        <TeamCard
+            imgSrc={team9}
+            name="Lina Jutila"
+            title="Web Developer"
+          />
+
+          <TeamCard
+            imgSrc={team11}
+            name="Lina Jutila"
+            title="Web Developer"
+          />
+
+          <TeamCard
+            imgSrc={team13}
             name="Lina Jutila"
             title="Web Developer"
           />
