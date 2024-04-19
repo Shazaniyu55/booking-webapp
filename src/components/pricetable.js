13a7c5c84c4bfbd9ccc3e9ada8b1a5d99806bd30
Shazaniyu55 <shazaniyu@gmail.com>
2024-04-19 15:18:24 +0100

    niyu2@@

--- src/components/pricetable.js
+++ src/components/pricetable.js
@@ -11,20 +11,9 @@ function Table(){
         <thead className="text-xs text-white uppercase bg-black1 dark:bg-gray-700 dark:text-white">
             <tr>
                 <th scope="col" className="px-6 py-3">
-                    Product name
+                    THE BASIC
                 </th>
-                <th scope="col" className="px-6 py-3">
-                    Color
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Category
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Price
-                </th>
-                <th scope="col" className="px-6 py-3">
-                    Action
-                </th>
+                
             </tr>
         </thead>
         <tbody>
